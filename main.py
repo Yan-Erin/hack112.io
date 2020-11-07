d785008cfda53280dfe1ef27d972987c39c2c526
Nancy <nzzuo@andrew.cmu.edu>
2020-11-07 10:04:21 -0500

    Revert "fixed quiting :("
    This reverts commit 550f990cc3775b8bdb23d492675e64fc3b497a5b.

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import Walls
 pygame.init()
 screen = pygame.display.set_mode((1010, 510))
 done = False
-while not done:
-    for event in pygame.event.get():
-        Walls.redrawAll(screen)
-        if event.type == pygame.QUIT:
-            done = True
-        pygame.display.flip()
+for event in pygame.event.get():
+    Walls.redrawAll(screen)
+    if event.type == pygame.QUIT:
+        done = True
+    pygame.display.flip()
 pygame.quit()
