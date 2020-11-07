550f990cc3775b8bdb23d492675e64fc3b497a5b
glowyRectangle <68751020+glowyRectangle@users.noreply.github.com>
2020-11-07 09:09:55 -0500

    fixed quiting :(

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import Walls
 pygame.init()
 screen = pygame.display.set_mode((1010, 510))
 done = False
-for event in pygame.event.get():
-    Walls.redrawAll(screen)
-    if event.type == pygame.QUIT:
-        done = True
-    pygame.display.flip()
+while not done:
+    for event in pygame.event.get():
+        Walls.redrawAll(screen)
+        if event.type == pygame.QUIT:
+            done = True
+        pygame.display.flip()
 pygame.quit()
