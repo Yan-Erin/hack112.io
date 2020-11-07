500235abed9b154e6802cd36bf71321155022858
mattngaw <73301816+mattngaw@users.noreply.github.com>
2020-11-07 18:30:41 -0500

    weapons

--- weapons.py
+++ weapons.py
@@ -1,12 +1,26 @@
+import pygame
+import os
+
 class Projectile(pygame.sprite.Sprite):
 
     def __init__(self, x, y, direction):
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        pygame.sprite.Sprite.__init__(self)
         self.direction = direction
         self.dx = None
         self.dy = None
+        self.damage = 10
+        self.speed = 5
+        self.images = []
+        for i in range(4):
+            img = pygame.image.load(os.path.join("images", f"{self.name}{str(i)}.png"))
+            self.images.append(img)
+        if self.direction == [0, 1]: self.image = self.images[0]
+        if self.direction == [0, -1]: self.image = self.images[3]
+        if self.direction == [1, 0]: self.image = self.images[1]
+        if self.direction == [-1, 0]: self.image = self.images[2]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
     def move(self):
         pass
@@ -15,16 +29,12 @@ class Projectile(pygame.sprite.Sprite):
 class Plane(Projectile):
 
     def __init__(self, x, y, direction):
-        self.damage = 5
-        self.speed = 5
-        self.image = pygame.image.load(os.path.join("images", ".png"))
+        self.name = "plane"
         Projectile.__init__(self, x, y, direction)
 
 
-class Book(Projectile):
+class Pencil(Projectile):
 
     def __init__(self, x, y, direction):
-        self.damage = 10
-        self.speed = 5
-        self.image = pygame.image.load(os.path.join("images", ".png"))
+        self.name = "pencil"
         Projectile.__init__(self, x, y, direction)
