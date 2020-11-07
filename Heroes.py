ca9e9bb56e0636d72679c1d85c4ddc45f7baee1b
mattngaw <73301816+mattngaw@users.noreply.github.com>
2020-11-07 11:30:50 -0500

    Merge branch 'heroes' into main

---
+++ Heroes.py
@@ -0,0 +1,43 @@
+import pygame
+import os
+import math
+
+class Hero(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = 0
+        self.y = 0
+        self.dx = 0
+        self.dy = 0
+        self.speed = 2
+        self.health = 100
+        self.armor = 0
+        self.direction = [0, 0] # dx, dy
+        img = pygame.image.load(os.path.join("sprites", "test.png"))
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.y = 0
+
+    def move(self):
+        angles = [
+            [None,  270,    90],
+            [0,     315,    45],
+            [180,   225,    135]
+        ]
+        i, j = self.direction
+        angle = angles[i][j] * math.pi / 180
+        self.dx = self.speed * math.cos(angle)
+        self.dy = self.speed * math.sin(angle)
+        print(f"dx = {self.dx}, dy = {self.dy}")
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+
+class Kosbie(Hero):
+
+    def specialAbility(self):
+        pass
+
+class Taylor(Hero):
+    def specialAbility(self):
+        pass
--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import pygame
-import Walls
+import Walls, Heroes
+
 pygame.init()
 screen = pygame.display.set_mode((1010, 510))
 done = False
-for event in pygame.event.get():
+FPS = 60
+clock = pygame.time.Clock()
+
+player1 = Heroes.Kosbie()
+player1.rect.x = 100
+player1.rect.y = 100
+player_list = pygame.sprite.Group()
+player_list.add(player1)
+
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            pygame.quit()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]: player1.direction[1] = 1
+    elif keys[pygame.K_s]: player1.direction[1] = -1
+    else: player1.direction[1] = 0
+    if keys[pygame.K_d]: player1.direction[0] = 1
+    elif keys[pygame.K_a]: player1.direction[0] = -1
+    else: player1.direction[0] = 0
+
+    if player1.direction != [0, 0]: player1.move()
+
+    screen.fill((0, 0, 0))
     Walls.redrawAll(screen)
-    if event.type == pygame.QUIT:
-        done = True
+    player1.update()
+    player_list.draw(screen)
     pygame.display.flip()
-pygame.quit()
+    clock.tick(FPS)
